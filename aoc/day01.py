2d2af054dadce8d26b24fd0844364d7ad9a2ed79
George Murray <george@benchling.com>
2021-12-02 15:33:25 -0800

    Day 1 part 2

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,14 +1,17 @@
-from logging import INFO
+from typing import Generator
 
 from aoc import cache
 
 
 class Solution:
-    def run(self) -> int:
+    def yield_ints(self) -> Generator[int, None, None]:
+        for line in cache.input_for_day(1).lines():
+            yield int(line)
+
+    def run_part_1(self) -> int:
         previous = None
         result = 0
-        for line in cache.input_for_day(1).lines():
-            val = int(line)
+        for val in self.yield_ints():
             if previous is None:
                 previous = val
                 continue
@@ -17,6 +20,19 @@ class Solution:
             previous = val
         return result
 
+    def run_part_2(self) -> int:
+        vals = self.yield_ints()
+        current = [next(vals), next(vals), next(vals)]
+        result = 0
+        for val in vals:
+            current_sum = sum(current)
+            current = current[1:] + [val]
+            next_sum = sum(current)
+            if next_sum > current_sum:
+                result += 1
+        return result
+
 
 if __name__ == "__main__":
-    print(Solution().run())
+    print(Solution().run_part_1())
+    print(Solution().run_part_2())
