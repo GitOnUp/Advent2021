41ba16e304fe1d3823cc6dbd227f4114085b991e
George Murray <murray.george.m@gmail.com>
2021-12-28 21:59:24 -0500

    Add diagonal neighbors to grid

--- aoc/grid.py
+++ aoc/grid.py
@@ -4,6 +4,9 @@ from aoc import cache
 
 
 class Grid:
+    CARDINAL_NEIGHBOR_DELTAS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    ALL_NEIGHBOR_DELTAS = CARDINAL_NEIGHBOR_DELTAS + [(1, 1), (-1, -1), (1, -1), (-1, 1)]
+
     def __init__(self, lines: List[List[int]]):
         self.lines = lines
 
@@ -17,9 +20,10 @@ class Grid:
             for x in range(len(self.lines[y])):
                 yield x, y
 
-    def valid_neighbors(self, x, y) -> List[Tuple[int, int]]:
+    def valid_neighbors(self, x, y, cardinal_only=True) -> List[Tuple[int, int]]:
         neighbors = []
-        for delta_x, delta_y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        deltas = self.CARDINAL_NEIGHBOR_DELTAS if cardinal_only else self.ALL_NEIGHBOR_DELTAS
+        for delta_x, delta_y in deltas:
             new_x, new_y = x + delta_x, y + delta_y
             if self.at(new_x, new_y) is not None:
                 neighbors.append((new_x, new_y))
